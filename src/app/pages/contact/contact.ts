4879887abeddd3ea6e56e64d38779cde7d26f7ba
nurulaiini <aininazli2410@gmail.com>
2025-11-25 23:54:45 +0800

    Initial Angular resume site

---
+++ src/app/pages/contact/contact.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  imports: [],
+  templateUrl: './contact.component.html' +
+    '',
+  styleUrl: './contact.css',
+})
+export class Contact {
+
+}
